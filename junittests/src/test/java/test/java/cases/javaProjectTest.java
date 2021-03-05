d37850ab5a9b073b0070ee0d8ad9823ae87bc4d5
midhunvprasad <66428332+midhunvprasad@users.noreply.github.com>
2021-03-04 22:36:20 -0500

    Update javaProjectTest.java
    Updated and added  4 more testcases

--- junittests/src/test/java/test/java/cases/javaProjectTest.java
+++ junittests/src/test/java/test/java/cases/javaProjectTest.java
@@ -55,6 +55,8 @@ class javaProjectTest {
 			int actual = jp.ReverseNumber(1234);
 			assertEquals(actual,expected,"Should show actual reversed number");
 	}
+	
+
 	@Test
 	@Tag("String")
 	@DisplayName("Testing String")
@@ -65,5 +67,51 @@ class javaProjectTest {
 			char out = jp.stringcheck(str, index);
 			assertEquals('c',out,"Check for the character in index " +index);
 	}
+	
+	@Nested
+	class power{
+		
+	@Test
+	@Tag("Math")
+	@DisplayName("Testing Power of a number")
+	void testpower() {
+			javaProject jp=new javaProject();
+			int expected=32;
+			int actual = jp.calculatePower(2,5);
+			assertEquals(actual,expected,"Should show actual power of the number");
+	}
+	@Test
+	@Tag("Math")
+	@DisplayName("Testing Power of a number(with one number negative)")
+	void testpowerNegative() {
+			javaProject jp=new javaProject();
+			int expected=1024;
+			int actual = jp.calculatePower(-2,10);
+			assertEquals(actual,expected,"Should show actual power of the number");
+	}
+	}
+	
 
+	@Nested
+	class Triangle{
+	@Test
+	@Tag("Math")
+	@DisplayName("test Area of Triangle")
+	void testAreaofTriangle() {
+			javaProject jp=new javaProject();
+			double expected=4;
+			double actual = jp.AreaTriangleDemo(4, 2);
+			assertEquals(actual,expected,"Should show actual area of Triangle");
+	}
+	@Test
+	@Tag("Math")
+	@DisplayName("Area of triangle with more values")
+	void testpowerNegative() {
+			javaProject jp=new javaProject();
+			 assertAll(
+				      "values",
+				      () -> assertEquals(30,jp.AreaTriangleDemo(6, 10), "Should show actual area of Triangle"),
+				      () -> assertEquals(8, jp.AreaTriangleDemo(2, 8),"Should show actual area of Triangle"));
+	}
+	}
 }
